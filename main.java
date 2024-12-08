937ee06aca180543684ac95cca16cd049efeb7e4
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-12-08 19:15:42 +0300

    rename file

--- main.java
+++ main.java
@@ -1,4 +1,4 @@
-public class main {
+public class Main {
   public static void main(String[] args) {
     ContainsDuplicate cd = new ContainsDuplicate();
     System.out.println(cd.containsDuplicate(new int[]{1, 2, 3, 4, 5}));
