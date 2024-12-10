cf223c28ae81cf8662ae3a169356cef18991a256
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-12-10 15:36:11 +0300

    GroupAnagrams

--- 49-group-anagrams.java
+++ 49-group-anagrams.java
@@ -1,8 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 class GroupAnagrams {
   // "eat", "tea", "tan", "ate", "nat", "bat"
   public List<List<String>> groupAnagrams(String[] strs) {
-    return null;
+    HashMap<String, List<String>> str2ascii = new HashMap<>();
+
+    for (String str : strs) {
+      String ascii = getStrAscii(str);
+
+      if (str2ascii.containsKey(ascii)) {
+        str2ascii.get(ascii).add(str);
+      } else {
+        List<String> newList = new ArrayList<>();
+        newList.add(str);
+        str2ascii.put(ascii, newList);
+      }
+    }
+
+    return new ArrayList<>(str2ascii.values());
+  }
+
+  private String getStrAscii(String str) {
+    char[] chars = str.toCharArray();
+    Arrays.sort(chars);
+    StringBuilder ascii = new StringBuilder();
+    for (char ch : chars) {
+      ascii.append((int) ch);
+    }
+    return ascii.toString();
   }
 }
