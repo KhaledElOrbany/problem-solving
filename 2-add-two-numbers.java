c90e8f3359d03e4213e21a1fb1368404e9860e19
Khaled Elorbany <khaled.wagdy554@gmail.com>
2024-12-15 21:26:12 +0300

    addTwoNumbers

--- 2-add-two-numbers.java
+++ 2-add-two-numbers.java
@@ -15,32 +15,65 @@ class ListNode {
   }
 }
 
+class LinkedListManipulation {
+  ListNode create(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return null;
+    }
+    ListNode head = new ListNode(nums[0]);
+    ListNode curr = head;
+
+    for (int i = 1; i < nums.length; i++) {
+      curr.next = new ListNode(nums[i]);
+      curr = curr.next;
+    }
+    return head;
+  }
+
+  void read(ListNode iter) {
+    System.out.println();
+    while (iter != null) {
+      System.out.print(iter.val);
+      iter = iter.next;
+    }
+    System.out.println();
+  }
+}
+
 class AddTwoNumbers {
   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-    long result = getLinkedListValue(l1) + getLinkedListValue(l2);
-    return createNewLinkedList(result);
-  }
+    ListNode head = null;
+    ListNode currentNode = head;
 
-  private long getLinkedListValue(ListNode l) {
-    StringBuilder value = new StringBuilder();
-    while (l != null) {
-      value.append(l.val);
-      l = l.next;
+    int carry = 0;
+    while (l1 != null || l2 != null) {
+      int l1Val = l1 != null ? l1.val : 0;
+      int l2Val = l2 != null ? l2.val : 0;
+
+      int currentSum = l1Val + l2Val + carry;
+      carry = currentSum / 10;
+      int lastDigit = currentSum % 10;
+
+      if (head == null) {
+        head = new ListNode(lastDigit);
+        currentNode = head;
+      } else {
+        currentNode.next = new ListNode(lastDigit);
+        currentNode = currentNode.next;
+      }
+
+      if (l1 != null) {
+        l1 = l1.next;
+      }
+      if (l2 != null) {
+        l2 = l2.next;
+      }
     }
 
-    return Long.parseLong(value.reverse().toString());
-  }
-
-  private ListNode createNewLinkedList(long value) {
-    char[] chars = String.valueOf(value).toCharArray();
-
-    ListNode l = null, temp = null;
-    for (int i = 0; i < chars.length; i++) {
-      temp = new ListNode(chars[i] - '0');
-      temp.next = l;
-      l = temp;
+    if (carry > 0) {
+      currentNode.next = new ListNode(carry);
     }
 
-    return l;
+    return head;
   }
 }
